00ff3c1770a3a6ffde9b246c88be1ff8760ce1d3
Carlo Oss Cazzador <carlo.osscazzador@studenti.unitn.it>
2025-06-10 12:03:32 +0200

    github aggiustamenti per il deploy

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,16 @@
 import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import vueDevTools from 'vite-plugin-vue-devtools'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    vueDevTools(),
-  ],
+  plugins: [vue()],
   base: '/TrentoWayVue/',
+  build: {
+    outDir: 'dist'
+  },
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
-
     },
   },
 })
